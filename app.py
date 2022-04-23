d0ba2416ab95c1ba55c7a12f2fcf359a1ae4a60c
Четвериков Иван Андреевич <ichetverikov@phoenixit.ru>
2022-04-23 17:00:21 +0300

    liquibase init db & docker-compose

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
+
 from flask import Flask
 from resources import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        host=os.environ.get("BACKEND_HOST", "127.0.0.1"),
+        port=os.environ.get("BACKEND_PORT"),
+    )
